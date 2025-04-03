c209eaaa5695ef281b7e84a132dd12ca5b692f1d
Everton Riconi <evertonriconi@gmail.com>
2025-04-03 16:11:24 -0300

    telas de registro de usuario e login mais metodos http

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  loginCard = true;
 
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  signupForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmitLoginForm(): void {
+    console.log('Dados de Login', this.loginForm.value);
+  }
+  onSubmitSignupForm(): void {
+    console.log('Dados de Registro', this.signupForm.value);
+  }
 }
---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { AuthResponse } from 'src/modules/interfaces/auth/AuthResponse';
+import { SignupUserRequest } from 'src/modules/interfaces/user/SignupUserRequest';
+import { SignupUserResponse } from 'src/modules/interfaces/user/SignupUserResponse';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private API_URL = environment.API_URL;
+
+  constructor(private http: HttpClient) {}
+
+  signupUser(requestDatas: SignupUserRequest): Observable<SignupUserResponse> {
+    return this.http.post<SignupUserResponse>(
+      `${this.API_URL}/user`,
+      requestDatas
+    );
+  }
+
+  authUser(requestDatas: AuthResponse): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.API_URL}/auth`, requestDatas);
+  }
+}
